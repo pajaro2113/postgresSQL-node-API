f3df2e77ed77e40be17f2ec493c0df2841b15e6f
pajaro2113 <alvarojavierpajarofernandez@gmail.com>
2024-09-10 10:03:12 -0500

    create routes and creating CRUD

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import express from 'express'
 import { PORT } from './config.js'
+import userRoutes from './routes/user.routes.js'
 
 const app = express()
+
+app.use(userRoutes);
 
 app.listen(PORT)
 console.log("server on port ",PORT)
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,1 +1,31 @@
 //definir url's, se puede agrupar, se puede crear url de usuario
+//rutas son las url que una aplicacion cliente o una aplicacion frontend o mobil puede llamar
+
+import { Router } from "express";
+
+const router = Router();
+
+router.get('/users',(req, res)=>{
+    res.send('Obteniendo todos los usuario')
+})
+
+router.get('/users/:id',(req, res)=>{
+    const {id} =req.params
+    res.send('Obteniendo usuario '+ id)
+})
+
+router.post('/users',(req, res)=>{
+    res.send('Creando usuario')
+})
+
+router.delete('/users/:id',(req, res)=>{
+    const {id} =req.params
+    res.send('Eliminando usuario '+id)
+})
+
+router.put('/users/:id',(req, res)=>{
+    const {id} =req.params
+    res.send('Actualizando usuario '+id)
+})
+
+export default router;
